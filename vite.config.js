3b69b3680bc6747d8721c035cdde0823e5f1192f
UnixUser <38821945+Unix-User@users.noreply.github.com>
2025-01-27 03:38:15 -0300

    package updates

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: 'resources/js/app.js',
+            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
         vue({
