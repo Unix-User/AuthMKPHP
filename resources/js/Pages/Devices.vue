df7cd33f0906b6880784dcb8295a08660f7ccc04
UnixUser <38821945+Unix-User@users.noreply.github.com>
2025-01-28 04:22:01 -0300

    changed language texts

--- resources/js/Pages/Devices.vue
+++ resources/js/Pages/Devices.vue
@@ -34,7 +34,7 @@ const fetchData = async () => {
         const response = await axios.get('/device');
         data.value = response.data.data;
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Erro ao buscar dados:', error);
     } finally {
         isLoading.value = false;
     }
@@ -89,7 +89,7 @@ const handleDevice = async (method) => {
         await fetchData();
         closeModal();
     } catch (error) {
-        console.error(`Error ${method === 'PUT' ? 'updating' : 'saving'} device:`, error);
+        console.error(`Erro ao ${method === 'PUT' ? 'atualizar' : 'salvar'} dispositivo:`, error);
     }
 };
 
@@ -104,7 +104,7 @@ const sync = async (device) => {
         await axios.get(`/device/${device.id}`);
         await fetchData(); // Refresh the device list after sync
     } catch (error) {
-        console.error("Error syncing device:", error);
+        console.error("Erro ao sincronizar dispositivo:", error);
     }
 };
 
@@ -116,22 +116,22 @@ const list = (device) => {
 };
 
 const deleteRow = async (device) => {
-    if (!confirm('Are you sure you want to delete this device?')) return;
+    if (!confirm('Tem certeza que deseja deletar este dispositivo?')) return;
     try {
         await axios.delete(`/device/${device.id}`);
         await fetchData();
     } catch (error) {
-        console.error("Error deleting device:", error);
+        console.error("Erro ao deletar dispositivo:", error);
     }
 };
 
 </script>
 
 <template>
-    <AppLayout title="Devices">
+    <AppLayout title="Dispositivos">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Devices
+                Dispositivos
             </h2>
         </template>
 
@@ -141,13 +141,13 @@ const deleteRow = async (device) => {
                     <div
                         class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
                         <h1 class="text-2xl font-medium text-gray-900 dark:text-white">
-                            Device Management
+                            Gerenciamento de Dispositivos
                         </h1>
 
                         <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-                            Here you can manage the devices in the system. You can add new devices, edit existing
-                            information,
-                            or remove devices.
+                            Aqui você pode gerenciar os dispositivos no sistema. Você pode adicionar novos dispositivos,
+                            editar
+                            informações existentes ou remover dispositivos.
                         </p>
                     </div>
 
@@ -155,23 +155,38 @@ const deleteRow = async (device) => {
                         class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 p-6 lg:p-8 space-y-6 md:space-y-0 md:grid md:grid-cols-2 md:gap-6 lg:gap-8">
                         <div>
                             <div class="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#809090" viewBox="0 0 512 512"
                                     stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0l-3-3m3 3l3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <g>
+                                            <g>
+                                                <path
+                                                    d="m163.3,87c51.1-49.6 134.4-49.7 185.5,0 11.3,11 24.9,3.7 28.9-0.5 7.9-8.1 7.6-21-0.4-28.9-66.9-64.6-175.6-64.6-242.5,0.1-8.1,7.9-8.3,20.8-0.4,28.9 7.8,8.1 20.8,8.3 28.9,0.4z">
+                                                </path>
+                                                <path
+                                                    d="m180.3,123.6c-8.1,7.9-8.3,20.8-0.4,28.9 7.8,8.1 20.8,8.3 28.9,0.5 26-25.3 67.9-24.7 94.5,0 11.4,10.6 24.9,3.7 28.9-0.5 7.9-8.1 7.6-21-0.4-28.9-41.9-40.4-109.7-40.4-151.5,0z">
+                                                </path>
+                                            </g>
+                                            <path
+                                                d="m399.6,255.8h-123.2v-41c0-11.3-9.1-20.4-20.4-20.4-11.3,0-20.4,9.1-20.4,20.4v41h-123.2c-55.9,0-101.4,45.5-101.4,101.6 0,55.3 44.5,100.4 99.5,101.4v21.8c0,11.3 9.1,20.4 20.4,20.4 11.3,0 20.4-9.1 20.4-20.4v-21.8h209.3v21.8c0,11.3 9.1,20.4 20.4,20.4 11.3,0 20.4-9.1 20.4-20.4v-21.8c55.1-1 99.5-46.1 99.5-101.6 0.1-55.9-45.4-101.4-101.3-101.4zm0,162.2h-287.2c-33.4,0-60.6-27.2-60.6-60.8 0-33.4 27.2-60.6 60.6-60.6h287.2c33.4,0 60.6,27.2 60.6,60.8-5.68434e-14,33.4-27.2,60.6-60.6,60.6z">
+                                            </path>
+                                        </g>
+                                    </g>
                                 </svg>
                                 <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">
-                                    <a href="#">Add New Device</a>
+                                    <a href="#">Adicionar Novo Dispositivo</a>
                                 </h2>
                             </div>
 
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Click the button below to add a new device to the system.
+                                Clique no botão abaixo para adicionar um novo dispositivo ao sistema.
                             </p>
 
                             <p class="mt-4 text-sm">
                                 <PrimaryButton @click="openModal()" class="mb-4">
-                                    Add New Device
+                                    Adicionar Novo Dispositivo
                                 </PrimaryButton>
                             </p>
                         </div>
@@ -184,12 +199,12 @@ const deleteRow = async (device) => {
                                         d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z" />
                                 </svg>
                                 <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">
-                                    <a href="#">Device List</a>
+                                    <a href="#">Lista de Dispositivos</a>
                                 </h2>
                             </div>
 
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Below is the list of all devices currently in the system.
+                                Abaixo está a lista de todos os dispositivos atualmente no sistema.
                             </p>
                         </div>
                     </div>
@@ -197,14 +212,14 @@ const deleteRow = async (device) => {
                     <div
                         class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
                         <div v-if="isLoading" class="text-center mt-12">
-                            <p>Loading...</p>
+                            <p>Carregando...</p>
                         </div>
                         <div v-else-if="data && data.length > 0">
-                            <DevicesTable :devicesData="data" @edit="edit" @delete="deleteRow"
-                                @list="list" @sync="sync" />
+                            <DevicesTable :devicesData="data" @edit="edit" @delete="deleteRow" @list="list"
+                                @sync="sync" />
                         </div>
                         <div v-else class="text-center mt-12">
-                            <p>No devices found.</p>
+                            <p>Nenhum dispositivo encontrado.</p>
                         </div>
                     </div>
 
@@ -217,19 +232,19 @@ const deleteRow = async (device) => {
                             <div class="bg-white dark:bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                 <div class="">
                                     <div class="mb-4">
-                                        <InputLabel for="name" value="Name" />
+                                        <InputLabel for="name" value="Nome" />
                                         <TextInput id="name" v-model="form.name" type="text" class="mt-1 block w-full"
                                             :disabled="viewMode" autocomplete="name" />
                                         <InputError :message="$page.props.errors.name" class="mt-2" />
                                     </div>
                                     <div class="mb-4">
-                                        <InputLabel for="ip" value="IP Address" />
+                                        <InputLabel for="ip" value="Endereço IP" />
                                         <TextInput id="ip" v-model="form.ip" type="text" :disabled="viewMode"
                                             class="mt-1 block w-full" autocomplete="ip" />
                                         <InputError :message="$page.props.errors.ip" class="mt-2" />
                                     </div>
                                     <div class="mb-4">
-                                        <InputLabel for="team_id" value="Team ID" />
+                                        <InputLabel for="team_id" value="ID da Equipe" />
                                         <TextInput id="team_id" v-model="form.team_id" type="text" :disabled="viewMode"
                                             class="mt-1 block w-full" autocomplete="team_id" />
                                         <InputError :message="$page.props.errors.team_id" class="mt-2" />
@@ -241,13 +256,13 @@ const deleteRow = async (device) => {
                                         <InputError :message="$page.props.errors.ikev2" class="mt-2" />
                                     </div>
                                     <div class="mb-4">
-                                        <InputLabel for="user" value="User" />
+                                        <InputLabel for="user" value="Usuário" />
                                         <TextInput id="user" v-model="form.user" type="text" :disabled="viewMode"
                                             class="mt-1 block w-full" autocomplete="user" />
                                         <InputError :message="$page.props.errors.user" class="mt-2" />
                                     </div>
                                     <div class="mb-4">
-                                        <InputLabel for="password" value="Password" />
+                                        <InputLabel for="password" value="Senha" />
                                         <TextInput id="password" v-model="form.password" type="password"
                                             :disabled="viewMode" class="mt-1 block w-full" autocomplete="password" />
                                         <InputError :message="$page.props.errors.password" class="mt-2" />
@@ -258,16 +273,16 @@ const deleteRow = async (device) => {
                                 <PrimaryButton type="button"
                                     class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5"
                                     v-show="!editMode && !viewMode" @click="handleDevice('POST')">
-                                    Save
+                                    Salvar
                                 </PrimaryButton>
                                 <PrimaryButton type="button"
                                     class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5"
                                     v-show="editMode" @click="handleDevice('PUT')">
-                                    Update
+                                    Atualizar
                                 </PrimaryButton>
                                 <SecondaryButton @click="closeModal()" type="button"
                                     class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white dark:bg-gray-800 text-base leading-6 font-medium text-gray-700 dark:text-gray-300 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                                    Cancel
+                                    Cancelar
                                 </SecondaryButton>
                             </div>
                         </form>
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -6,7 +6,7 @@ import UsersTable from '@/Components/UsersTable.vue';
 const props = defineProps(['data', 'errors']);
 
 const deleteRow = (data) => {
-    if (!confirm('Are you sure you want to delete this user?')) return;
+    if (!confirm('Tem certeza que deseja deletar este usuário?')) return;
     Inertia.delete(`/users/${data.id}`, {
         preserveState: true,
         preserveScroll: true,
@@ -15,10 +15,10 @@ const deleteRow = (data) => {
 </script>
 
 <template>
-    <AppLayout title="Users">
+    <AppLayout title="Usuários">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Users
+                Usuários
             </h2>
         </template>
 
@@ -28,13 +28,12 @@ const deleteRow = (data) => {
                     <div
                         class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
                         <h1 class="text-2xl font-medium text-gray-900 dark:text-white">
-                            User Management
+                            Gerenciamento de Usuários
                         </h1>
 
                         <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-                            Here you can manage the users of the system. You can invite new users, edit existing
-                            information, or
-                            remove users.
+                            Aqui você pode gerenciar os usuários do sistema. Você pode convidar novos usuários, editar informações existentes ou
+                            remover usuários.
                         </p>
                     </div>
 
@@ -48,17 +47,17 @@ const deleteRow = (data) => {
                                         d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                                 </svg>
                                 <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">
-                                    <a href="#">Add New User</a>
+                                    <a href="#">Adicionar Novo Usuário</a>
                                 </h2>
                             </div>
 
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Click the button below to invite a new user to the system.
+                                Clique no botão abaixo para convidar um novo usuário para o sistema.
                             </p>
 
                             <p class="mt-4 text-sm">
                                 <PrimaryButton @click="$inertia.get(route('teams.show', $page.props.auth.user.current_team_id)+'#manager')" class="mb-4">
-                                    Invite New User
+                                    Convidar Novo Usuário
                                 </PrimaryButton>
                             </p>
                         </div>
@@ -71,12 +70,12 @@ const deleteRow = (data) => {
                                         d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                                 </svg>
                                 <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">
-                                    <a href="#">User List</a>
+                                    <a href="#">Lista de Usuários</a>
                                 </h2>
                             </div>
 
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Below is the list of all users currently registered in the system.
+                                Abaixo está a lista de todos os usuários atualmente registrados no sistema.
                             </p>
                         </div>
                     </div>
